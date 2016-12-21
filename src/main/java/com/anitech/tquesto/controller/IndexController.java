bc831e2c6fd93670594ded536fa4c3bf5aabd97e
Tapas Jena <tapas.friends@gmail.com>
2016-12-21 22:26:57 +0800

    UI code added to the repo

--- src/main/java/com/anitech/tquesto/controller/IndexController.java
+++ src/main/java/com/anitech/tquesto/controller/IndexController.java
@@ -1,20 +1,28 @@
 package com.anitech.tquesto.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author Tapas
- *
  * This is the index controller required for SPA(Angular2) routing to work
  * 
+ * @author Tapas
+ * 
  */
-@RestController
+@Controller
 public class IndexController {
 
-	@RequestMapping("/")
-    String home() {
-        return "Hello Tapas!";
-    }
+	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+	
+	/**
+	 * This method is to let spring play nice with Angular Routes
+	 */
+	@RequestMapping({"/home/**"})
+	public String index() {
+		logger.info("Inside IndexController->index(), Forwarding request to index.html!");
+		return "forward:/index.html";
+	}
 
 }
