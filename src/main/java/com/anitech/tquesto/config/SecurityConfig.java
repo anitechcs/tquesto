99b9d016cd0bdfb3bb942aa86e23d57243e33880
Tapas Jena <tapas-pc@Tapass-MacBook-Pro.local>
2017-01-29 18:47:40 +0800

    Unwanted changes reverted back

--- src/main/java/com/anitech/tquesto/config/SecurityConfig.java
+++ src/main/java/com/anitech/tquesto/config/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and()
 			.authorizeRequests()
-			.antMatchers(HttpMethod.OPTIONS, "/api/**").permitAll() //Allow CORS preflight request
+			.antMatchers(HttpMethod.OPTIONS, "/**").permitAll() //Allow CORS preflight request
             .antMatchers("/*.{css,js,html}").permitAll()
             .antMatchers("/assets/**").permitAll()
 			.antMatchers("/api/register").permitAll()
